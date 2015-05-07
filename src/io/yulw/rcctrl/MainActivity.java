fafb5e0a2d5ea64742e29a124bf89d519f9502a5
repstd <wang_yuli@hotmail.com>
2015-05-07 23:37:00 +0800

    update:

--- src/io/yulw/rcctrl/MainActivity.java
+++ src/io/yulw/rcctrl/MainActivity.java
@@ -4,22 +4,82 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
+import android.widget.AdapterView.OnItemClickListener;
+import android.view.*;
 import io.yulw.test.*;
 import android.net.*;
 import android.net.wifi.*;
 import android.content.*;
 import android.util.*;
+import java.util.*;
+class worker extends Thread
+{
+	private rccontrol m_c;
+	private String m_msg;
+	public worker(rccontrol ctrl,String msg) {
+		m_c=ctrl;
+		m_msg=msg;
+	}
+	public void run() {
+		m_c.sendPacket(m_msg);
+	}
+}
 public class MainActivity extends Activity 
 {
 	private final String TAG="MainActivity.";
 	private WifiManager m_wifi=null;
+	private Switch m_switchOp;
+	private Button m_buttonSend;
+	private EditText m_editTextCmd;
+	private rccontrol m_ctrl;
+	private rcpara m_para;
+	private final int RCBROADCAST_PORT=6000;
+	private String m_cmd_name="null";
+	private String m_cmd_op="off";
+	private HashMap<String,String> m_mapRunningApps=new HashMap<String,String>();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		m_switchOp=(Switch)this.findViewById(R.id.switch_on_off);
+		m_switchOp.setChecked(false);
+		m_buttonSend=(Button)this.findViewById(R.id.button_send);
+		m_editTextCmd=(EditText)this.findViewById(R.id.editText1);
+		m_buttonSend.setOnClickListener(new View.OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+					String msg=encodeMsg();
+					Log.d(TAG,"Onclick.Msg to be sent: "+msg);
+					new worker(m_ctrl,msg).start();
+			}
+		});
+		m_switchOp.setOnCheckedChangeListener(new OnCheckedChangeListener()
+		{
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				// TODO Auto-generated method stub
+				if(isChecked) {
+					m_cmd_op="on";
+				}
+				else {
+					m_cmd_op="off";
+				}
+				Log.d(TAG,"Switch Changed to: "+m_cmd_op);
+			}
+		});
 		try {
-			m_wifi= (WifiManager) this
-					.getSystemService(Context.WIFI_SERVICE);
+			m_wifi= (WifiManager) this .getSystemService(Context.WIFI_SERVICE);
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -35,35 +95,46 @@ public class MainActivity extends Activity
 	}
 
 	@Override
-	protected void onStart() {
+	protected void onStart() 
+	{
 		// TODO Auto-generated method stub
 		super.onStart();
 		Log.d(TAG,"onStartIn");
-		//test
-        rcpara para=new rcpara(m_wifi,null,6000);
-        testClient test=new testClient(para);
-        test.start();
-		Log.d(TAG,"onStartExit");
+		m_para=new rcpara(m_wifi,null,RCBROADCAST_PORT);
+		m_ctrl=new rccontrol(m_para);
 	}
 
 	@Override
 	protected void onRestart() {
 		// TODO Auto-generated method stub
 		super.onRestart();
+		Log.d(TAG,"onRestart.");
 	}
 
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
+		Log.d(TAG,"onResume.");
+		if(m_ctrl.isDead())
+			m_ctrl.open();
 	}
-
 	@Override
-	protected void onStop() {
+	protected void onStop() 
+	{
 		// TODO Auto-generated method stub
 		super.onStop();
+		Log.d(TAG,"onStop.");
+		m_ctrl.close();
 	}
-
+	@Override
+	protected void onDestroy() 
+	{
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.d(TAG,"onDestroy.");
+		m_ctrl.close();
+	}
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
@@ -74,5 +145,26 @@ public class MainActivity extends Activity
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	public String encodeMsg()
+	{
+		Log.d(TAG,"eocodeMsg.current: "+m_cmd_name+" "+m_cmd_op);
+		m_cmd_name=this.m_editTextCmd.getText().toString();
+		/*@yulw,message to be sent to the rchost.
+		 *eg.  rcrender_on
+		 */
+		String msg;
+
+		if(m_mapRunningApps.get(m_cmd_name)==null&&m_cmd_op=="on") {
+			m_mapRunningApps.put(m_cmd_name,m_cmd_op);
+			msg=m_cmd_name+"_"+m_cmd_op;
+		}
+		else if(m_mapRunningApps.get(m_cmd_name)!=null&&m_cmd_op=="off"){
+			m_mapRunningApps.remove(m_cmd_name);
+			msg=m_cmd_name+"_"+m_cmd_op;
+		}
+		else
+			msg="";
+		return msg;
 	}
 }
--- src/io/yulw/rcctrl/rccontrol.java
+++ src/io/yulw/rcctrl/rccontrol.java
@@ -2,8 +2,7 @@ package io.yulw.rcctrl;
 import java.util.*;
 import java.net.*;
 
-import android.net.DhcpInfo;
-import android.net.wifi.WifiManager;
+import android.net.*;
 import android.util.Log;
 import android.content.*;
 import android.service.*;
@@ -20,30 +19,33 @@ public class rccontrol
     	m_para=para;
     	m_host=para.getHost();
     	m_port=para.getPort();
-    	try {
-			m_socket=new DatagramSocket(m_port);
-			m_socket.setBroadcast(true);
-			m_socket.setReuseAddress(true);
-			m_socket.setSoTimeout(1000);
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	open();
     }
     public rcpara getPara() {
     	return m_para;
     }
     public boolean sendPacket(String msg)
     {
-    	DatagramPacket packet=new DatagramPacket(msg.getBytes(),msg.length(),m_host,m_port);
+    	if(msg=="")
+    		return false;
     	try {
+    		DatagramPacket packet=new DatagramPacket(msg.getBytes(),msg.length(),m_host,m_port);
     		Log.d(TAG,"Sending data."+m_host.toString()+":"+m_port);
 			m_socket.send(packet);
-		} catch (IOException e) {
+		} 
+    	catch(SocketTimeoutException e) {
+			e.printStackTrace();
+			Log.d(TAG,"SocketTImedoutException in Sending Message.Error: "+e.getMessage());
+    	}
+    	catch(java.net.SocketException e) {
+
+			e.printStackTrace();
+			Log.d(TAG,"SocketException in Sending Message.Error: "+e.getMessage());
+    	}
+    	catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			Log.d(TAG,"Error in Sending Message.Error: "+e.getMessage());
-			return false;
+			Log.d(TAG,"IOError in Sending Message.Error: "+e.getMessage());
 		}
     	return true;
     }
@@ -59,5 +61,30 @@ public class rccontrol
 			return null;
 		}
     	return new String(packet.getData(),0,packet.getLength());
+    }
+    public void open()
+    {
+    	try {
+			m_socket=new DatagramSocket(m_port);
+			m_socket.setBroadcast(true);
+			m_socket.setReuseAddress(true);
+			m_socket.setSoTimeout(1000);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Log.d(TAG,"Error in Open Socket.Error: "+e.getMessage());
+		}
+    }
+    public boolean isDead() {
+    	return m_socket.isClosed();
+    }
+    public void close(){
+    	try {
+			if (!m_socket.isClosed())
+				m_socket.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			Log.d(TAG,"Error in Closing The Socket.Error: "+e.getMessage());
+		}	
     }
 }
